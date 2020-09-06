f1abe8d75113a0331a179c499d183c46ced0942e
Thanh Le <lenguyenthanh@hotmail.com>
2020-09-06 07:40:06 +0200

    Update vagrantfile and Hugo 0.74.3

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -1,24 +1,18 @@
 #!/bin/bash
-sudo apt-get update
-sudo apt-get upgrade --assume-yes
-sudo apt-get install --assume-yes git curl libstdc++6
+#sudo apt-get update
+#sudo apt-get upgrade --assume-yes
+#sudo apt-get install --assume-yes git
 
-HUGO_VERSION="0.60.1"
-HUGO_DOWNLOAD_URL="https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_extended_${HUGO_VERSION}_Linux-64bit.tar.gz"
+HUGO_VERSION="0.74.3"
+HUGO_DOWNLOAD_URL="https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_extended_${HUGO_VERSION}_Linux-64bit.deb"
 
 ## Install Hugo
-curl -L -o hugo.tgz $HUGO_DOWNLOAD_URL
-sudo tar -C /usr/bin -xzf hugo.tgz
-rm hugo.tgz
-
-# This workaround is required to avoid libstdc++ errors while running "extended" hugo with SASS support.
-curl -l -o libstdc++6 http://security.ubuntu.com/ubuntu/pool/main/g/gcc-5/libstdc++6_5.4.0-6ubuntu1~16.04.10_amd64.deb
-sudo dpkg --force-all -i libstdc++6
-rm libstdc++6
+curl -L -o hugo.deb $HUGO_DOWNLOAD_URL
+sudo dpkg -i hugo.deb
+rm hugo.deb
 
 chmod +x project/src/runserver.sh
 chmod +x project/src/build.sh
 
 # Go to project directory when ssh
 echo "cd project/src" >> /home/vagrant/.bashrc
-
